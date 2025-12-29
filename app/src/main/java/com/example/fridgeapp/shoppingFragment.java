77ddde39569db6de692b606f25f49a768e736e78
NuHazim <nufailhazim.sarifishak@gmail.com>
2025-12-29 18:29:25 +0800

    test je

--- app/src/main/java/com/example/fridgeapp/shoppingFragment.java
+++ app/src/main/java/com/example/fridgeapp/shoppingFragment.java
@@ -10,7 +10,7 @@ import android.view.ViewGroup;
 
 
 public class shoppingFragment extends Fragment {
-
+    //test je
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
